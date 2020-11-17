b24b173771fee86deede9d9aac13f2d8b3e29307
grzeski <grzegorz.rzeski@autonomik.pl>
2020-11-17 19:24:19 +0100

    added tests for exceptions

--- tests/PangramParameterizedTest.py
+++ tests/PangramParameterizedTest.py
@@ -15,6 +15,17 @@ class PangramParametrizedFile(unittest.TestCase):
                 value, expected = (data[0], data[1].strip("\n"))
                 self.assertEqual(pangramClass.game(value), expected)
 
+    def test_from__file_exception(self):
+        file = open("../data/pangram_tests_exception")
+        pangramClass = IsPangram()
+        for line in file:
+            if line.startswith("#") or line.startswith(" ") or line.startswith("\n"):
+                continue
+            else:
+                data = line.split(" ")
+                value, expected = (data[0], data[1].strip("\n"))
+                self.assertRaisesRegex(Exception, expected, pangramClass.game, int(value))
+
 
 if __name__ == '__main__':
     unittest.main()
